62f56331e030db5a9afda56e2c8e1387e8fd4dc5
Kemal Sogut <kevinsst20@gmail.com>
2025-04-05 12:57:07 -0400

    projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,11 +2,26 @@
 export const projectsData = [
   {
     id: "blinds-nisa",
-    title: 'BlindsNisa Website and Ad',
-    description: 'A fully responsive e-commerce platform with product filtering, cart management, and secure checkout.',
-    image: 'https://drive.google.com/file/d/1KuQ-zS7zHqeqKZo4zoIHt2Bye4l1_LYz/view?usp=sharing',
+    title: 'BlindsNisa Website and Admin Panel',
+    description: 'A front page to welcome and capture leads of customers as well as a comprehensive web application for managing blinds sales, including an admin panel for inventory and order management.',
+    image: 'https://drive.google.com/file/d/1KuQ-zS7zHqeqKZo4zoIHt2Bye4l1_LYz/preview',
     technologies: ['Node.js', 'MongoDB', 'Express', 'Jquery', 'Typed.js'],
     category: 'web',
-
+  },
+  {
+    id: "fit-track",
+    title: 'Fit Track Pro',
+    description: 'A fitness application that allows users to track their workouts, nutrition, and progress over time.',
+    image: 'https://drive.google.com/file/d/1KuQ-zS7zHqeqKZo4zoIHt2Bye4l1_LYz/preview',
+    technologies: ['React','Node.js', 'MongoDB', 'Express', 'AWS',],
+    category: 'web',
+  },
+  {
+    id: "fit-track",
+    title: 'Fit Track Pro',
+    description: 'A front page to welcome and capture leads of customers as well as a comprehensive web application for managing blinds sales, including an admin panel for inventory and order management.',
+    image: 'https://drive.google.com/file/d/1KuQ-zS7zHqeqKZo4zoIHt2Bye4l1_LYz/preview',
+    technologies: ['React','Node.js', 'MongoDB', 'Express', 'AWS',],
+    category: 'web',
   },
 ];
